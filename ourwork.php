0195005a7ac39fdb1d1db1482c9e2cbd5d86e426
williamsclovis2 <33696262+williamsclovis2@users.noreply.github.com>
2020-10-29 18:20:12 +0200

    first commit

---
+++ ourwork.php
@@ -0,0 +1,248 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <title>Cube communication</title>
+  <link rel="stylesheet" href="fonts/opensans/stylesheet.css">
+  <link rel="stylesheet" href="fonts/bebas/stylesheet.css">
+  <link rel="stylesheet" href="fonts/ionicons.min.css">
+  <link rel="stylesheet" href="fonts/font-awesome.min.css">
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="js/vendor/swiper.min.css">
+  <link rel="stylesheet" href="js/vendor/jquery.fullpage.min.css">
+  <link rel="stylesheet" href="js/vegas/vegas.min.css">
+
+  <!-- Main CSS files -->
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/custom.css">
+  <link rel="stylesheet" href="css/aos.css">
+  <!-- add alt layout here -->
+  <link rel="stylesheet" href="css/style-default.css">
+
+  <script src="js/vendor/modernizr-2.7.1.min.js"></script>
+  <link rel="stylesheet" href="css/animate.css">
+</head>
+
+<body id="menu" class="body-page">
+
+
+  <!-- BEGIN OF site header Menu -->
+   <?php include("header.php");?>
+  <!-- END OF site header Menu-->
+
+  <main class="page-main page-fullpage main-anim" id="mainpage">
+
+    <!-- Begin of home section -->
+   
+    <div class="section section-list-feature fp-auto-height-responsive no-m-padng " data-section="1" id="s1">
+      <div class="section-wrapper fullwidth with-margin">
+        <!-- content -->
+        <div class="section-content anim">
+          <div class="col-12 col-lg-12">
+              <div class="row  custom-album">
+                 <div class="col-md-3">
+                    <div class="content">
+                        <a href="#" data-toggle="modal" data-target="#afsld-model">
+                          <div class="content-overlay"></div>
+                          <img src="front/lun.jpg" class="content-image" style="width:100%;">
+                          <div class="content-details fadeIn-bottom">
+                            <h3 class="content-title">Lufthansa| Kigali | Rwanda </h3>
+                          </div>
+                        </a>
+                    </div>
+                 </div>
+                 <div class="col-md-3">
+                    <div class="content">
+                        <a href="#" data-toggle="modal" data-target="#afsld-model">
+                          <div class="content-overlay"></div>
+                          <img src="front/afsld.jpg" class="content-image" style="width:100%;">
+                          <div class="content-details fadeIn-bottom">
+                            <h3 class="content-title">Africa Food Security Leadership Dialogue | Kigali | Rwanda </h3>
+                          </div>
+                        </a>
+                    </div>
+                 </div>
+                 <div class="col-md-3">
+                  <div class="content">
+                        <a href="#" data-toggle="modal" data-target="#orf-model">
+                          <div class="content-overlay"></div>
+                          <img src="front/orf_two.jpg" class="content-image" style="width:100%;">
+                          <div class="content-details fadeIn-bottom">
+                            <h3 class="content-title">Kigali Global Dialogue 2019 |Kigali |Rwanda  </h3>
+                          </div>
+                        </a>
+                    </div>
+                 </div>
+                 <div class="col-md-3">
+                    <div class="content">
+                        <a href="#" data-toggle="modal" data-target="#mymodal">
+                          <div class="content-overlay"></div>
+                          <img src="front/Ef.jpg" class="content-image" style="width:100%;">
+                          <div class="content-details fadeIn-bottom">
+                            <h3 class="content-title">Eisenhower Fellowships Africa Regional Conference 2019 |Kigali |Rwanda  </h3>
+                          </div>
+                        </a>
+                    </div>
+                 </div>
+                  <div class="col-md-3">
+                    <div class="content">
+                        <a href="#" data-toggle="modal" data-target="#kwibuka-model">
+                          <div class="content-overlay"></div>
+                          <img src="front/25Kwibuka.jpg" class="content-image" style="width:100%;">
+                          <div class="content-details fadeIn-bottom">
+                            <h3 class="content-title">25th Commemoration of the 1994 Genocide against the Tutsi | Kigali | Rwanda</h3>
+                          </div>
+                        </a>
+                    </div>
+                 </div>
+                 <div class="col-md-3">
+                  <div class="content">
+                        <a href="#" data-toggle="modal" data-target="#dubai-model">
+                          <div class="content-overlay"></div>
+                          <img src="front/1-SME.jpg" class="content-image" style="width:100%;">
+                          <div class="content-details fadeIn-bottom">
+                            <h3 class="content-title">Dubai Expo 2020 World Majlis | Kigali | Rwanda </h3>
+                          </div>
+                        </a>
+                    </div>
+                 </div> 
+                <div class="col-md-3">
+                  <div class="content">
+                        <a href="#" data-toggle="modal" data-target="#bea-model">
+                          <div class="content-overlay"></div>
+                          <img src="front/1BEA.jpg" class="content-image" style="width:100%;">
+                          <div class="content-details fadeIn-bottom">
+                            <h3 class="content-title">Business Excellence Awards <br>2019 | Kigali | Rwanda</h3>
+                          </div>
+                        </a>
+                    </div>
+                 </div>
+                <div class="col-md-3">
+                  <div class="content">
+                        <a href="#" data-toggle="modal" data-target="#myModal-BEA">
+                          <div class="content-overlay"></div>
+                          <img src="front/IAFT.jpg" class="content-image" style="width:100%;">
+                          <div class="content-details fadeIn-bottom">
+                            <h3 class="content-title">Intra Africa Trade Fair Conference 2018 | Cairo | Egypt </h3>
+                          </div>
+                        </a>
+                    </div>
+                 </div>
+                 <div class="col-md-3">
+                  <div class="content">
+                        <a href="#" data-toggle="modal" data-target="#myModal-BEA">
+                          <div class="content-overlay"></div>
+                          <img src="front/IAFT.jpg" class="content-image" style="width:100%;">
+                          <div class="content-details fadeIn-bottom">
+                            <h3 class="content-title">Intra Africa Trade Fair Conference 2018 | Cairo | Egypt </h3>
+                          </div>
+                        </a>
+                    </div>
+                 </div>
+                
+              </div>
+          </div>
+        </div>
+       <footer class="section-footer scrolldown">
+          <a class="down">
+            <i class="ion ion-chevron-down"></i>
+          </a>
+        </footer>
+      </div>
+      <!-- End of section wrapper -->
+    </div>
+  </main>
+  <!-- END OF page main content -->
+<!--  Section model-->
+<section id="kwibuka">
+    <div class="modal fade modal-fullscreen" id="mymodal" role="dialog" style="background-color:#000;">
+        <div class="modal-dialog modal-lg" style="boeder-raduis">
+            <div class="modal-content" style="box-shadow:none; border:none;">
+                <div class="model-body">
+                    <div class="container-fluid">
+                        <a class="close" data-dismiss="modal"><img src="closeicon.png"></a>
+                    <div class="row">
+                    <div class="col-md-12">
+                      <div id="carousel-custom-16" class="carousel slide" data-ride="carousel" data-interval="false" >
+                      <!-- Wrapper for slides -->
+                          <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                              <img src="images/ourwork/EF/ef1.JPG" alt="..." class="img-responsive">
+                                 <div class="carousel-caption" id="cap1">
+                                    <h3>Eisenhower Fellowships Africa Regional Conference 2019 |Kigali |Rwanda</h3>
+                                    <p>Concept development | Website building | Branding | Advertising &amp; Communications Services | Media Management | Participant Management |Technical Production planning &amp; execution| Stage design &amp; construction | Provision of Sound, Lighting &amp; AV equipment| Filming &amp;&amp; Live Feed | Transport Management | Accommodation Management | Airport Meet &amp; Greet | Catering Management | Social activities| Delegate tour activities</p>
+                                 </div>
+
+                            </div>
+                           <div class="item">
+                              <img src="images/ourwork/EF/ef2.jpg" alt="..." class="img-responsive">
+                                   <div class="carousel-caption" id="cap1">
+                                    <h3>Eisenhower Fellowships Africa Regional Conference 2019 |Kigali |Rwanda</h3>
+                                    <p>Concept development | Website building | Branding | Advertising &amp; Communications Services | Media Management | Participant Management |Technical Production planning &amp; execution| Stage design &amp; construction | Provision of Sound, Lighting &amp; AV equipment| Filming &amp;&amp; Live Feed | Transport Management | Accommodation Management | Airport Meet &amp; Greet | Catering Management | Social activities| Delegate tour activities</p>
+                                 </div>
+
+                            </div>
+                            <div class="item">
+                              <img src="images/ourwork/EF/ef3.JPG" alt="..." class="img-responsive">
+                                 <div class="carousel-caption" id="cap1">
+                                    <h3>Eisenhower Fellowships Africa Regional Conference 2019 |Kigali |Rwanda</h3>
+                                    <p>Concept development | Website building | Branding | Advertising &amp; Communications Services | Media Management | Participant Management |Technical Production planning &amp; execution| Stage design &amp; construction | Provision of Sound, Lighting &amp; AV equipment| Filming &amp;&amp; Live Feed | Transport Management | Accommodation Management | Airport Meet &amp; Greet | Catering Management | Social activities| Delegate tour activities</p>
+                                 </div>
+
+                            </div>
+                            <div class="item">
+                              <img src="images/ourwork/EF/ef4.JPG" alt="..." class="img-responsive">
+                                  <div class="carousel-caption" id="cap1">
+                                    <h3>Eisenhower Fellowships Africa Regional Conference 2019 |Kigali |Rwanda</h3>
+                                    <p>Concept development | Website building | Branding | Advertising &amp; Communications Services | Media Management | Participant Management |Technical Production planning &amp; execution| Stage design &amp; construction | Provision of Sound, Lighting &amp; AV equipment| Filming &amp;&amp; Live Feed | Transport Management | Accommodation Management | Airport Meet &amp; Greet | Catering Management | Social activities| Delegate tour activities</p>
+                                 </div>
+
+                            </div>
+
+                          </div>
+
+                          <!-- Controls -->
+                          <a class="left carousel-control" href="#carousel-custom-16" role="button" data-slide="prev">
+                            <i class="fa fa-long-arrow-left"></i>
+                            <span class="sr-only">Previous</span>
+                          </a>
+                          <a class="right carousel-control" href="#carousel-custom-16" role="button" data-slide="next">
+                            <i class="fa fa-long-arrow-right"></i>
+                            <span class="sr-only">Next</span>
+                          </a>
+
+
+                          <!-- Indicators -->
+                          <ol class="carousel-indicators">
+                              <li data-target="#carousel-custom-16" data-slide-to="0" class="active">
+                                 <img src="images/ourwork/EF/ef1.JPG" alt="..." class="img-responsive">
+                              </li>
+                              <li data-target="#carousel-custom-16" data-slide-to="1">
+                                 <img src="images/ourwork/EF/ef2.jpg" alt="..." class="img-responsive">
+                              </li>
+                              <li data-target="#carousel-custom-16" data-slide-to="2">
+                                 <img src="images/ourwork/EF/ef3.JPG" alt="..." class="img-responsive">
+                              </li>
+                               <li data-target="#carousel-custom-16" data-slide-to="3">
+                                 <img src="images/ourwork/EF/ef4.JPG" alt="..." class="img-responsive">
+                              </li>
+
+                          </ol> 
+                       </div>
+                      </div>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/vendor/jquery-1.12.4.min.js"></script>
+  <script src="js/slick/slick.min.js"></script>
+  <script src="js/vendor/all.js"></script>
+  <script src="js/main.js"></script>
+</body>
+
+</html>
